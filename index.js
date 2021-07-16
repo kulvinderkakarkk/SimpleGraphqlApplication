e6562dcb36f46b9e2ec2258c19878e4e07c7e4ca
kulvinder kakar <kulvinderkakar14@gmail.com>
2021-07-16 19:53:51 +1000

    Added functionality for Schema and resovlers for graphql

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require('express')
+import ItemSchema from '../SimpleGraphqlApplication/Schema/item.js'
+import {graphqlHTTP} from 'express-graphql'
+import express  from 'express'
+import ItemResolver from '../SimpleGraphqlApplication/Resolver/item.js'
+
 
 const app=express()
-
 app.get("/", (req,res) => {
     res.send("The application has started")
 })
+
+const root = ItemResolver
+
+app.use('/graphql', graphqlHTTP({
+    schema: ItemSchema,
+    rootValue: root,
+    graphiql: true
+}))
 
 app.listen(8082, () => {console.log("Application running at 8082")})
